fcbe5bb261d20bac159c1b948a48a9597740cae3
Andrew Card <mrsaltinecracker1@gmail.com>
2021-03-26 16:56:18 -0700

    File Structure

---
+++ src/main/java/com/shalin/pan/client/util/ClientUtils.java
@@ -0,0 +1,8 @@
+package com.shalin.pan.client.util;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+@OnlyIn(Dist.CLIENT)
+public class ClientUtils {
+}
---
+++ src/main/java/com/shalin/pan/core/init/EnchantmentInit.java
@@ -0,0 +1,4 @@
+package com.shalin.pan.core.init;
+
+public class EnchantmentInit {
+}
